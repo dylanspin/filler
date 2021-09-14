2357bb1b2ba37a69fa278d26d1ae78e1f6a76f04
dylanspin <47520114+dylanspin@users.noreply.github.com>
2021-09-14 12:14:07 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let count = 0
 let rows = 0
 basic.forever(function () {
+    led.unplot(count - 1, rows - 0)
     led.plot(count, rows)
     if (count >= 4) {
         if (rows >= 4) {
@@ -8,6 +9,8 @@ basic.forever(function () {
             basic.pause(200)
             basic.clearScreen()
         } else {
+            basic.pause(100)
+            led.unplot(count, rows)
             rows = rows + 1
         }
         count = 0
